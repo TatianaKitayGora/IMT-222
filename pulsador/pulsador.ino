797331ab14101f2e6364c192911db703b0ff0806
TatianaKitayGora <tkitaygorab@gmail.com>
2024-08-23 11:50:08 -0400

    Pulsador que enciende un led

---
+++ pulsador/pulsador.ino
@@ -0,0 +1,14 @@
+#define LED 2
+#define PUL 4
+void setup  (){
+    pinMode(LED,OUTPUT);
+    pinMode(PUL,INPUT);
+}
+void loop () {
+    if(digitalRead(PUL)){
+        digitalWrite(LED,55);
+
+    } else{
+        digitalWrite(LED,0);
+    }
+}
